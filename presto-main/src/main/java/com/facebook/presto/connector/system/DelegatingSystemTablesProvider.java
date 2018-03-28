0bff5c6df24ddeb64d7fad2892986fb74ca382e0
Łukasz Osipiuk <lukasz@osipiuk.net>
2018-03-28 16:19:38 +0200

    fixup! Use DelegatingSystemTablesProvider in ConnectorManager

--- presto-main/src/main/java/com/facebook/presto/connector/system/DelegatingSystemTablesProvider.java
+++ presto-main/src/main/java/com/facebook/presto/connector/system/DelegatingSystemTablesProvider.java
@@ -19,7 +19,6 @@ import com.facebook.presto.spi.SystemTable;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
