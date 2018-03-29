e9c71f7aa4b2ff0419af798ba60db02ab97d3925
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-03-29 09:49:33 +0200

    Add support for non-default aggregations, limit and OR in subqueries

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PlanNodeDecorrelator.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PlanNodeDecorrelator.java
@@ -19,16 +19,22 @@ import com.facebook.presto.sql.planner.PlanNodeIdAllocator;
 import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.SymbolsExtractor;
 import com.facebook.presto.sql.planner.iterative.Lookup;
+import com.facebook.presto.sql.planner.plan.AggregationNode;
 import com.facebook.presto.sql.planner.plan.Assignments;
 import com.facebook.presto.sql.planner.plan.FilterNode;
+import com.facebook.presto.sql.planner.plan.LimitNode;
 import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.PlanVisitor;
 import com.facebook.presto.sql.planner.plan.ProjectNode;
+import com.facebook.presto.sql.tree.ComparisonExpression;
 import com.facebook.presto.sql.tree.Expression;
+import com.facebook.presto.sql.tree.SymbolReference;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -36,6 +42,7 @@ import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static com.facebook.presto.sql.tree.ComparisonExpressionType.EQUAL;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.util.Objects.requireNonNull;
 
@@ -75,7 +82,9 @@ public class PlanNodeDecorrelator
             return Optional.of(new DecorrelationResult(
                     node,
                     ImmutableSet.of(),
-                    ImmutableList.of()));
+                    ImmutableList.of(),
+                    ImmutableMap.of(),
+                    false));
         }
 
         @Override
@@ -99,7 +108,98 @@ public class PlanNodeDecorrelator
             return Optional.of(new DecorrelationResult(
                     newFilterNode,
                     Sets.difference(SymbolsExtractor.extractUnique(correlatedPredicates), ImmutableSet.copyOf(correlation)),
-                    correlatedPredicates));
+                    correlatedPredicates,
+                    extractCorrelatedSymbolsMapping(correlatedPredicates),
+                    false));
+        }
+
+        @Override
+        public Optional<DecorrelationResult> visitLimit(LimitNode node, Void context)
+        {
+            Optional<DecorrelationResult> childDecorrelationResultOptional = lookup.resolve(node.getSource()).accept(this, null);
+            if (!childDecorrelationResultOptional.isPresent() || node.getCount() == 0) {
+                return Optional.empty();
+            }
+
+            DecorrelationResult childDecorrelationResult = childDecorrelationResultOptional.get();
+            if (childDecorrelationResult.atMostSingleRow) {
+                return childDecorrelationResultOptional;
+            }
+
+            if (correlation.isEmpty() || !childDecorrelationResult.getConstantSymbols().containsAll(node.getOutputSymbols())) {
+                return Optional.empty();
+            }
+
+            // Rewrite to aggregation on symbols which we know are constants and are also part of output
+            childDecorrelationResultOptional = new AggregationNode(
+                    idAllocator.getNextId(),
+                    node.getSource(),
+                    ImmutableMap.of(),
+                    ImmutableList.of(ImmutableList.<Symbol>builder()
+                            .addAll(correlation)
+                            .addAll(node.getOutputSymbols())
+                            .build()),
+                    AggregationNode.Step.SINGLE,
+                    Optional.empty(),
+                    Optional.empty()).accept(this, null);
+            if (!childDecorrelationResultOptional.isPresent() || !childDecorrelationResultOptional.get().atMostSingleRow) {
+                return Optional.empty();
+            }
+
+            return childDecorrelationResultOptional;
+        }
+
+        @Override
+        public Optional<DecorrelationResult> visitAggregation(AggregationNode node, Void context)
+        {
+            if (node.hasEmptyGroupingSet()) {
+                return Optional.empty();
+            }
+
+            Optional<DecorrelationResult> childDecorrelationResultOptional = lookup.resolve(node.getSource()).accept(this, null);
+            if (!childDecorrelationResultOptional.isPresent()) {
+                return Optional.empty();
+            }
+
+            DecorrelationResult childDecorrelationResult = childDecorrelationResultOptional.get();
+            Set<Symbol> constantSymbols = childDecorrelationResult.getConstantSymbols();
+
+            AggregationNode decorrelatedAggregation = new SymbolMapper(childDecorrelationResult.correlatedSymbolsMapping)
+                    .map(node, childDecorrelationResult.node);
+
+            ImmutableList.Builder<List<Symbol>> newGroupingSets = ImmutableList.builder();
+            for (List<Symbol> groupingSet : decorrelatedAggregation.getGroupingSets()) {
+                List<Symbol> symbolsToAdd = childDecorrelationResult.symbolsToPropagate.stream()
+                        .filter(symbol -> !groupingSet.contains(symbol))
+                        .collect(toImmutableList());
+
+                if (!constantSymbols.containsAll(symbolsToAdd)) {
+                    return Optional.empty();
+                }
+
+                newGroupingSets.add(ImmutableList.<Symbol>builder()
+                        .addAll(groupingSet)
+                        .addAll(symbolsToAdd).build());
+            }
+
+            AggregationNode newAggregation = new AggregationNode(
+                    decorrelatedAggregation.getId(),
+                    decorrelatedAggregation.getSource(),
+                    decorrelatedAggregation.getAggregations(),
+                    newGroupingSets.build(),
+                    decorrelatedAggregation.getStep(),
+                    decorrelatedAggregation.getHashSymbol(),
+                    decorrelatedAggregation.getGroupIdSymbol());
+
+            boolean atMostSingleRow = newAggregation.getGroupingSets().size() == 1
+                    && constantSymbols.containsAll(newAggregation.getGroupingKeys());
+
+            return Optional.of(new DecorrelationResult(
+                    newAggregation,
+                    childDecorrelationResult.symbolsToPropagate,
+                    childDecorrelationResult.correlatedPredicates,
+                    childDecorrelationResult.correlatedSymbolsMapping,
+                    atMostSingleRow));
         }
 
         @Override
@@ -123,7 +223,39 @@ public class PlanNodeDecorrelator
             return Optional.of(new DecorrelationResult(
                     new ProjectNode(idAllocator.getNextId(), childDecorrelationResult.node, assignments),
                     childDecorrelationResult.symbolsToPropagate,
-                    childDecorrelationResult.correlatedPredicates));
+                    childDecorrelationResult.correlatedPredicates,
+                    childDecorrelationResult.correlatedSymbolsMapping,
+                    childDecorrelationResult.atMostSingleRow));
+        }
+
+        private Map<Symbol, Symbol> extractCorrelatedSymbolsMapping(List<Expression> conjuncts)
+        {
+            Map<Symbol, Symbol> mapping = new HashMap<>();
+            for (Expression conjunct : conjuncts) {
+                if (!(conjunct instanceof ComparisonExpression)) {
+                    continue;
+                }
+
+                ComparisonExpression comparison = (ComparisonExpression) conjunct;
+                if (!(comparison.getLeft() instanceof SymbolReference
+                        && comparison.getRight() instanceof SymbolReference
+                        && comparison.getType().equals(EQUAL))) {
+                    continue;
+                }
+
+                Symbol left = Symbol.from(comparison.getLeft());
+                Symbol right = Symbol.from(comparison.getRight());
+
+                if (correlation.contains(left) && !correlation.contains(right)) {
+                    mapping.put(left, right);
+                }
+
+                if (correlation.contains(right) && !correlation.contains(left)) {
+                    mapping.put(right, left);
+                }
+            }
+
+            return ImmutableMap.copyOf(mapping);
         }
 
         private Predicate<Expression> isUsingPredicate(List<Symbol> symbols)
@@ -138,11 +270,23 @@ public class PlanNodeDecorrelator
         final Set<Symbol> symbolsToPropagate;
         final List<Expression> correlatedPredicates;
 
-        public DecorrelationResult(PlanNode node, Set<Symbol> symbolsToPropagate, List<Expression> correlatedPredicates)
+        final Map<Symbol, Symbol> correlatedSymbolsMapping;
+        // If a subquery has at most single row for any correlation values?
+        final boolean atMostSingleRow;
+
+        DecorrelationResult(PlanNode node, Set<Symbol> symbolsToPropagate, List<Expression> correlatedPredicates, Map<Symbol, Symbol> correlatedSymbolsMapping, boolean atMostSingleRow)
         {
             this.node = node;
             this.symbolsToPropagate = symbolsToPropagate;
             this.correlatedPredicates = correlatedPredicates;
+            this.atMostSingleRow = atMostSingleRow;
+            this.correlatedSymbolsMapping = correlatedSymbolsMapping;
+        }
+
+        Set<Symbol> getConstantSymbols()
+        {
+            // Returns constants symbols from a perspective of a subquery
+            return ImmutableSet.copyOf(correlatedSymbolsMapping.values());
         }
     }
 
